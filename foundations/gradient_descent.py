236b883f1cc2142b09249fd86f9b150e0a0e3dd7
Kuntal Pal <kpal002@ucr.edu>
2026-04-17 19:01:52 -0700

    Update Gradient descent optimizer (Math Foundations 1/6)

--- foundations/gradient_descent.py
+++ foundations/gradient_descent.py
@@ -5,7 +5,6 @@ class Solution:
         # Update rule:        x = x - learning_rate * f'(x)
         # Round final answer to 5 decimal places
         x_old = init
-        x_new = 0
         for i in range(iterations):
             x_new = x_old - 2*learning_rate*x_old
             x_old = x_new
